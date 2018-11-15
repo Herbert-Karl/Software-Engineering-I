ecbf9603c91b9f29f66be0d534cf44bb0fd23419
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2018-11-15 23:38:02 +0100

    added interfaces

---
+++ stowage_cargo/ICargoSystem.java
@@ -0,0 +1,27 @@
+package stowage_cargo;
+
+import java.util.ArrayList;
+
+public interface ICargoSystem {
+
+    public String version();
+
+    public void load(Stowage stowage, Container container, FrontStowagePositionID position);
+
+    public void load(Stowage stowage, AirCargoPallet airCargoPallet, RearStowagePositionID position);
+
+    public double determineTotalAirCargoPallet(Stowage stowage);
+
+    public double determineTotalWeightContainer(Stowage stowage);
+
+    public void unlock();
+
+    public void secure();
+
+    public void lock();
+
+    public ArrayList<Container> unloadContainer(Stowage stowage);
+
+    public ArrayList<AirCargoPallet> unloadAirCargoPallet(Stowage stowage);
+
+}
---
+++ stowage_cargo/IStowageNumberFive.java
@@ -0,0 +1,7 @@
+package stowage_cargo;
+
+public interface IStowageNumberFive {
+
+    //
+    public int load(Container container);
+}
