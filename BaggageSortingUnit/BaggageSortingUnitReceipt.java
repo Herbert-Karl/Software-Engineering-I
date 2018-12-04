cbc6c815cfa984b4da01a4f44b0ffd2f8ec24f62
Jonas <j.hasenauer98@gmail.com>
2018-12-04 20:26:56 +0100

    Adding Final Models

---
+++ BaggageSortingUnit/BaggageSortingUnitReceipt.java
@@ -0,0 +1,14 @@
+package BaggageSortingUnit;
+
+public class BaggageSortingUnitReceipt {
+    private String uuid;
+    private int numberOfBaggageScanned;
+    private int numberOfDangerousBaggage;
+    private DestinationBox destinationBox;
+    private int numberOfBaggageFirstClass;
+    private int numberOfBaggageBusinessClass;
+    private int numberOfBaggageEconomyClass;
+    private int numberOfContainerNormalBaggage;
+    private int numberOfContainerBulkyBaggage;
+
+}
