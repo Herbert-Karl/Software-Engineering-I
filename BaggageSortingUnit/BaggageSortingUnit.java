f32a5957b311c238afdd7ddbeab21147fb8e7701
sIPeye <fobytobi@googlemail.com>
2018-12-04 20:31:30 +0100

    added Class properties

--- BaggageSortingUnit/BaggageSortingUnit.java
+++ BaggageSortingUnit/BaggageSortingUnit.java
@@ -1,6 +1,22 @@
 package BaggageSortingUnit;
+import java.util.ArrayList;
+
+
 
 public class BaggageSortingUnit implements IBaggageSortingUnit {
+    private ArrayList<Employee> employeeList;
+    private ArrayList<String> scanPatternList;
+    private IBaggageScanner baggageScanner;
+    private IBaggageSortingUnitRoboter roboter;
+    private BaggageDepot baggageDepot;
+    private DestinationBox destinationBox;
+    private ArrayList<LuggageTub> emptyLuggageTubList;
+    private ArrayList<Container> emptyContainerList;
+    private ArrayList<Container> filledContainerList;
+    private IBaggageVehicle baggageVehicle;
+    private ICustoms customs;
+
+
     @java.lang.Override
     public void executeRequest(GateID gateID) {
 
