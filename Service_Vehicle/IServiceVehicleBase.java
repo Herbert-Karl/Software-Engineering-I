8a49b3cbc30709713aea8306940ab1fc4ff81cfc
Supporterino <lars@roth-kl.de>
2018-12-05 17:39:33 +0100

    Adding IServiceVehicleBase Interface

---
+++ Service_Vehicle/IServiceVehicleBase.java
@@ -0,0 +1,17 @@
+public interface IServiceVehicleBase {
+    void executeRequest(GateID gateID);
+    void setFlashingLightOn();
+    void move(int speedInMPH);
+    void stop();
+    void setGateID(GateID gateID);
+    void connectToAirplane();
+    void increaseLevel(IAPUOilTank apuOilTank);
+    void charge(IBattery battery);
+    void increaseLevel(IEngineOilTank engineOilTank);
+    void change(IFireExtinguisher fireExtinguisher);
+    void refill(IDeIcingSystem deIcingSystem);
+    void disconnectFromAirplane();
+    void setFlashingLightOff();
+    void notifyGroundOperations(ServiceVehicleBaseReceipt serviceVehicleBaseReceipt);
+    void returnToAirportResourcePool();
+}
