82c01e77164a4aa494b210755209a368fc4c0211
Tabeavv <tabeavv@gmx.de>
2018-12-05 15:04:01 +0100

    updated baggage

--- Base/CabinBaggage.java
+++ Base/CabinBaggage.java
@@ -1,3 +1,8 @@
 public class CabinBaggage extends Baggage{
-
+    public CabinBaggage(String uuid, String content, double weight){
+        this.uuid = uuid;
+        this.content = content;
+        this.weight = weight;
+        this.securityStatus = null;
+    }
 }
