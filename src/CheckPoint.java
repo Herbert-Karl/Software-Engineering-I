4812198c2eaa6f2b9547000409839f874bf46269
Blackswater <Raphaellawo@gmx.de>
2018-12-05 10:32:59 +0100

    unnötiger ordner gelöscht

--- src/CheckPoint.java
+++
@@ -1,6 +0,0 @@
-public class CheckPoint {
-    private int sequenceId;
-    private String name;
-    private String latitude;
-    private String longitude;
-}
--- src/SeatManagementSortCriteria.java
+++
@@ -1,5 +0,0 @@
-public enum SeatManagementSortCriteria {
-    SortBySeat,
-    SportByTicketClass,
-    SortByName
-}
