9ee968c7c79d34ff1d63f2969b39512f16e50d78
Supporterino <lars@roth-kl.de>
2018-12-05 20:06:49 +0100

    Added Base Structure of Team 06

---
+++ src/Airport/Sky_Tanking_Vehicle/FuelPump.java
@@ -0,0 +1,55 @@
+package Airport.Sky_Tanking_Vehicle;
+
+import Airplane.Tanks.FuelTank;
+import Airplane.Tanks.IFuelTank;
+import Airport.Base.AirportFuelTank;
+
+public class FuelPump {
+    private boolean isOn;
+    private AirportFuelTank connectedAirportFuelTank;
+    private IFuelTank connectedFuelTank;
+
+    public FuelPump() {
+
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public AirportFuelTank getConnectedAirportFuelTank() {
+        return connectedAirportFuelTank;
+    }
+
+    public IFuelTank getConnectedFuelTank() {
+        return connectedFuelTank;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    public void setConnectedAirportFuelTank(AirportFuelTank connectedAirportFuelTank) {
+        this.connectedAirportFuelTank = connectedAirportFuelTank;
+    }
+
+    public void setConnectedFuelTank(IFuelTank connectedFuelTank) {
+        this.connectedFuelTank = connectedFuelTank;
+    }
+
+    public void on(){
+
+    }
+
+    public void connectAirportFuelTank(AirportFuelTank airportFuelTank) {
+
+    }
+
+    public void connectFuelTank(FuelTank fuelTank) {
+
+    }
+
+    public void off() {
+
+    }
+}
