1e8b2202ba8f744fe6b64241022036605fb2c310
Tabeavv <tabeavv@gmx.de>
2018-12-05 12:19:09 +0100

    test

---
+++ Base/AirportFuelTank.java
@@ -0,0 +1,9 @@
+public class AirportFuelTank{
+    private int id;
+    private int amount;
+
+    public AirportFuelTank(int id, int amount){
+        this.id = id;
+        this.amount = amount;
+    }
+}
