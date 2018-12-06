d15a9e9aa5a4e410a728ed28544ed2648342a1ea
XaNNy0 <XaNNy0@outlook.de>
2018-12-06 16:11:53 +0100

    UI added

---
+++ src/UI/Main.java
@@ -0,0 +1,26 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Parent root = loader.load();
+        primaryStage.setTitle("Airport");
+        Scene scene = new Scene(root, 820.0, 484.0);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
