a0748cf5e2b20a8a197d716d89534065aeea7fee
Tabeavv <tabeavv@gmx.de>
2018-12-07 13:28:03 +0100

    updated methods

--- src/Airport/Airport/AirportResourcePool.java
+++ src/Airport/Airport/AirportResourcePool.java
@@ -33,18 +33,12 @@ public class AirportRecourcePool{
         employeeList = new ArrayList(anzahl);
         //set "constants"
         int zaehler = 1000;
-        UUID employeeUUID = null;
-        UUID idCardUUID = null;
         String name = "Max Mustermann";
 /       //generate anzahl
         for(int i = 0; i<anzahl; i++){
             //Generate Employee and IDCard
-            employeeUUID = UUID.randomUUID();
-            idCardUUID = UUID.randomUUID();
-            String uuid_employee = employeeUUID.toString();
-            String uuid_card = idCardUUID.toString();
-            if((zaehler%2 != 0)){gender = FEMALE;} else {gender = MALE; }  //Abwechselnd MALE und FEMALE
-            IDCard idCard = new IDCard(uuid_card);
+            if((zaehler%2) != 0){gender = FEMALE;} else {gender = MALE; }  //Abwechselnd MALE und FEMALE
+            IDCard idCard = new IDCard();
             Employee employee = new Employee(uuid_employee, zähler, name, gender);
             idCard.setEmployee(employee);
             employee.setIDCard(idCard);
@@ -56,61 +50,116 @@ public class AirportRecourcePool{
     }
 
     private void generateAirCargoPalletLifter(int anzahl){
-        //set length of lists
         airCargoPalletLifterList = new ArrayList(anzahl);
-        int zaehler = 2000;
+
+        int zaehler = 1500;
         UUID palletLifterUUID = null;
-        //TODO
         //Type?
-        //Mit for-Schleife generieren
-        //Put on ArrayList
+        for(int i = 0; i<anzahl; i++){
+            palletLifterUUID = UUID.randomUUID();
+            String uuidLifter = palletLifterUUID.toString();
+            //NEED TO SET PARAMETERS
+           AirCargoPalletLifter lifter = new AirCargoPalletLifter();
+            airCargoPalletLifterList.add(lifter);
+        }
     }
 
     private void generateAirCargoPalletVehicle(int anzahl){
-        //set length of list
+
         airCargoPalletVehicleList = new ArrayList(anzahl);
+        int zaehler = 2000;
+        UUID palletVehicleUUID = null;
+        //Mit Schleife generieren
+        for(int i = 0; i<anzahl; i++){
+            palletVehicleUUID = UUID.randomUUID;
+            String uuidPalletVehicle = palletVehicleUUID.toString();
+            //NEED TO SET PARAMETERS
+           AirCargoPalletVehicle palletVehicle = new AirCargoPalletVehicle();
+           //In Liste schreiben
+        }
         //TODO
     }
 
     private void generateContainerLifter(int anzahl){
-        //set length of list
         containerLifterList = new ArrayList(anzahl);
+        int zaehler = 2500;
+        //type?
+        for(int i = 0; i<anzahl; i++){
+           //NEED TO SET PARAMETERS
+           ContainerLifter containerLifter = new ContainerLifter();
+            containerLifterList.add(containerLifter);
+        }
 /       //TODO
     }
 
     private void generateBaggageVehicle(int anzahl){
-        //set length of list
         baggageVehicleList = new ArrayList(anzahl);
+        int zaehler = 3000;
+        //type?
+        for(int i = 0; i<anzahl; i++){
+           //NEED TO SET PARAMETERS
+            BaggageVehicle baggageVehicle = new BaggageVehicle();
+            baggageVehicleList.add(baggageVehicle);
+        }
         //TODO
     }
 
     private void generateServiceVehicleBase(int anzahl){
-        //set length of list
         serviceVehicleBaseList = new ArrayList(anzahl);
+        int zaehler = 3500;
+        for(int i = 0; i<anzahl; i++){
+            //NEED TO SET PARAMETERS
+            ServiceVehicleBase vehicleBase = new ServiceVehicleBase();
+            serviceVehicleBaseList.add(vehicleBase);
+        }
         //TODO
     }
 
     private void generateServiceVehicleFreshWater(int anzahl){
-        //set length of list
         serviceVehicleFreshWaterList = new ArrayList(anzahl);
+        int zaehler = 4000;
+        for(int i = 0; i<anzahl; i++){
+           //NEED TO SET PARAMETERS
+           ServiceVehicleFreshWater freshWater = new ServiceVehicleFreshWater();
+           serviceVehicleFreshWaterList.add(freshWater);
+        }
         //TODO
     }
 
     private void generateServiceVehicleNitrogenOxygen(int anzahl){
         //set length of list
         serviceVehicleNitrogenOxigenList = new ArrayList(anzahl);
+        int zaehler = 4500;
+        //type?
+        for(int i = 0; i<anzahl; i++){
+            //NEED TO SET PARAMETERS
+           ServiceVehicleNitrogenOxygen nitrogenOxygen = new ServiceVehicleNitrogenOxygen();
+           serviceVehicleNitrogenOxigenList.add(nitrogenOxygen);
+        }
         //TODO
     }
 
     private void generateServiceVehicleWasteWater(int anzahl){
         //set length of list
         serviceVehicleWasteWaterList = new ArrayList(anzahl);
+        int zaehler = 5000;
+        for(int i = 0; i<anzahl; i++){
+            //NEED TO SET PARAMETERS
+            ServiceVehicleWasteWater wasteWater = new serviceVehicleWastewater();
+            serviceVehicleWasteWaterList.add(wasteWater);
+        }
         //TODO
     }
 
     private void generateSkyTankingVehicle(int anzahl){
         //set length of list
         skyTankingVehicleList = new ArraylList(anzahl);
+        int zaehler = 5500;
+        for(int i = 0; i<anzahl; i++){
+            //NEED TO SET PARAMETERS
+            SkyTankingVehicle skyTankingVehicle = new SkyTankingVehicle();
+            skyTankingVehicleList.add(skyTankingVehicle);
+        }
         //TODO
     }
 
