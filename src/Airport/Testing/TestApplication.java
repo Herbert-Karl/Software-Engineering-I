d8d508da095dc6b5b932ffcbadced2481647b814
chiarakramer <cici-kramer@hotmail.de>
2018-12-08 12:46:29 +0100

    testAirport added

--- src/Airport/Testing/TestApplication.java
+++ src/Airport/Testing/TestApplication.java
@@ -1,21 +1,32 @@
 package Airport.Testing;
 
+import Airport.Airport.Airport;
+import Airport.Airport.AirportRecourcePool;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestApplication {
 
     private TestAirplane testAirplane;
+    private Airport testAirport;
+
 
     @BeforeAll
     public void setup(){
         testAirplane = new TestAirplane(1, "abc", false);
+        testAirport = new Airport();
     }
 
     @Test
-    public void test01(){
+    public void testAirportAttributes(){
 
-        assertEquals(testAirplane.id, 3);
+        //assertEquals(testAirplane.id, 3);
+        assertEquals(testAirport.getPassengerList(), testPassengerList);
+        assertEquals(testAirport.getRecourcePool(), )
+
     }
 }
