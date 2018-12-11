cd031ed5abd35c0a9a4c5707026da4d0e0fcef67
fpoh <fabianpohlink@gmx.de>
2018-12-11 21:28:24 +0100

    Test

---
+++ Tests/Airplane/Management/RouteManagementTest.java
@@ -0,0 +1,36 @@
+package Airplane.Management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouteManagementTest {
+
+    @Test
+    void version() {
+    }
+
+    @Test
+    void on() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void printCheckPoints() {
+    }
+
+    @Test
+    void setCostIndex() {
+    }
+
+    @Test
+    void off() {
+    }
+}
