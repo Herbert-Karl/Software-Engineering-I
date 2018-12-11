c7192d7c4f7175c251d75f71a2125dc49e04ad93
Supporterino <lars@roth-kl.de>
2018-12-11 18:13:25 +0100

    Added Configuration

--- src/Airport/configuration/Configuration.java
+++ src/Airport/configuration/Configuration.java
@@ -1,7 +1,39 @@
 package Airport.configuration;
 
-public enum Configuration {
-    dataFilePath,
-    logFilePath,
-    source
+import Airport.base.Source;
+
+public class Configuration {
+    private String dataFilePath;
+    private String logFilePath;
+    private Source source;
+
+    public Configuration(String dataFilePath, String logFilePath) {
+        this.dataFilePath = dataFilePath;
+        this.logFilePath = logFilePath;
+        this.source = Source.MUC;
+    }
+
+    public String getDataFilePath() {
+        return dataFilePath;
+    }
+
+    public void setDataFilePath(String dataFilePath) {
+        this.dataFilePath = dataFilePath;
+    }
+
+    public String getLogFilePath() {
+        return logFilePath;
+    }
+
+    public void setLogFilePath(String logFilePath) {
+        this.logFilePath = logFilePath;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
 }
