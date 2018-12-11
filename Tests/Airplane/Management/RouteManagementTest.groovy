8d5e69d75ca7bbe3122ca05a69ca7d4b22742ca3
fpoh <fabianpohlink@gmx.de>
2018-12-11 21:05:05 +0100

    Test

---
+++ Tests/Airplane/Management/RouteManagementTest.groovy
@@ -0,0 +1,24 @@
+package Airplane.Management
+
+class RouteManagementTest extends GroovyTestCase {
+    void testVersion() {
+    }
+
+    void testOn() {
+    }
+
+    void testAdd() {
+    }
+
+    void testRemove() {
+    }
+
+    void testPrintCheckPoints() {
+    }
+
+    void testSetCostIndex() {
+    }
+
+    void testOff() {
+    }
+}
