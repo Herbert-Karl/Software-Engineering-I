73b1c44b11b77386d119b3d5dc604e7fa85b954d
fpoh <fabianpohlink@gmx.de>
2018-12-12 22:08:46 +0100

    Test

--- Tests/Airplane/Management/RouteManagementTest.java
+++ Tests/Airplane/Management/RouteManagementTest.java
@@ -1,10 +1,11 @@
 package Airplane.Management;
 
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RouteManagementTest {
+class RouteManagementTest extends TestCase {
 
     @Test
     void version() {
