9fe2e8d707d3235578b5c3c7039609ccf6433e9e
nemtudom123 <37618040+nemtudom123@users.noreply.github.com>
2018-12-12 13:27:37 +0100

    class "APU"

---
+++ src/Airplane/APU_Engine_Pump_Gear/Interface_APU.java
@@ -0,0 +1,60 @@
+interface IAPU {
+	public String version();
+	public void start();
+	public void increaseRPM(int value);
+	public void decreaseRPM(int value);
+	public void shutdown();
+}
+
+class APU implements IAPU {
+	String manufacturer;
+	String type;
+	String id;
+	boolean isStarted;
+	int rpm;
+	
+	public String version(){
+		return "<" + id + "> - <" + type + ">";		
+	}
+	
+	public void start(){
+		if(isStarted == false){
+			isStarted = true;
+		}
+	}
+	
+	public int increaseRPM(int value){
+		int x = rpm;
+		if((rpm + value) < 6500){
+			for(int i = x; i < (x+value); i++){
+				rpm = i;
+			}
+		}
+		else{
+			for(int i = x; i < (6500); i++){
+				rpm = i;
+			}
+		}
+	}
+	
+	public int decreaseRPM(int value){
+		int x = rpm;
+		if((rpm - value) > 0){
+			for(int i = x; i > (x-value); i--){
+				rpm = i;
+			}
+		}
+		else{
+			for(int i = x; i > 0; i--){
+				rpm = i;
+			}
+		}
+	}
+	
+	public boolean shutdown(){
+		isStarted = false;
+		while(rpm > 0){
+			rpm--;	
+		}	
+	}
+}
