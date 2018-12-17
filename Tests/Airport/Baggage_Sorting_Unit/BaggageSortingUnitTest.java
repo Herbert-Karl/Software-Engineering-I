9aa3e912e2872a7d6e98129627026e81f3fa0749
Lars <fgfhg@gdhga.com>
2018-12-17 17:58:37 +0100

    BaggageSortingUniTest added

---
+++ Tests/Airport/Baggage_Sorting_Unit/BaggageSortingUnitTest.java
@@ -0,0 +1,60 @@
+package Airport.Baggage_Sorting_Unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaggageSortingUnitTest {
+
+    @Test
+    void executeRequest() {
+    }
+
+    @Test
+    void loginBaggageScanner() {
+    }
+
+    @Test
+    void logoutBaggageScanner() {
+    }
+
+    @Test
+    void scan() {
+    }
+
+    @Test
+    void handOverToCustoms() {
+    }
+
+    @Test
+    void throwOff() {
+    }
+
+    @Test
+    void emptyDestinationBox() {
+    }
+
+    @Test
+    void sendContainerLifterToGate() {
+    }
+
+    @Test
+    void optimizeAirplaneLoading() {
+    }
+
+    @Test
+    void loadBaggageVehicle() {
+    }
+
+    @Test
+    void sendBaggageVehicleToGate() {
+    }
+
+    @Test
+    void notifyGroundOperations() {
+    }
+
+    @Test
+    void returnEmptyLuggageTubToCheckInDesk() {
+    }
+}
