87d1111d7886898e0b7a6241fb1d9a46ad607324
Tabeavv <tabeavv@gmx.de>
2018-12-18 08:13:08 +0100

    implemented reading device

--- src/Airport/Checkin_Desk/ReadingDevice.java
+++ src/Airport/Checkin_Desk/ReadingDevice.java
@@ -1,4 +1,28 @@
 package Airport.Checkin_Desk;
 
+import java.util.UUID;
+
 public class ReadingDevice {
+    private String uuid;
+    private String type;
+    private String id;
+
+    public ReadingDevice(String type, String id){
+        UUID uuid = UUID.randomUUID();
+        this.uuid = uuid.toString();
+        this.type = type;
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
