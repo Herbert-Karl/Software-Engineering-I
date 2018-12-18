197beec8a5941d09de458bbae229a08f7133d6da
Lars <fgfhg@gdhga.com>
2018-12-18 16:10:00 +0100

    New Tests Files and Getter Methods

--- Tests/Airport/Baggage_Sorting_Unit/BaggageSortingUnitTest.java
+++ Tests/Airport/Baggage_Sorting_Unit/BaggageSortingUnitTest.java
@@ -1,10 +1,36 @@
 package Airport.Baggage_Sorting_Unit;
 
+import Airport.Base.*;
+import Airport.Customs.Customs;
+import Airport.Scanner.BaggageScanner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaggageSortingUnitTest {
+    private BaggageSortingUnit baggageSortingUnit;
+    private ArrayList<Baggage> baggageList;
+    private BaggageScanner baggageScanner;
+
+    @BeforeEach
+    void setup() {
+        baggageList = new ArrayList<Baggage>();
+        baggageScanner = new BaggageScanner();
+
+        for (int i = 0; i < 10; i++) {
+            baggageList.add(new NormalBaggage("content", 100,
+                    new BaggageIdentificationTag(null, "flight", 100, null, "barcode" + i)));
+        }
+
+        baggageSortingUnit = new BaggageSortingUnit(new ArrayList<Employee>(), baggageScanner, new DestinationBox(null, baggageList,
+                100), new Customs());
+    }
 
     @Test
     void executeRequest() {
@@ -16,10 +42,18 @@ class BaggageSortingUnitTest {
 
     @Test
     void logoutBaggageScanner() {
+        baggageSortingUnit.logoutBaggageScanner();
+        assertNull(baggageScanner.getEmployee());
     }
 
     @Test
-    void scan() {
+    void scan() throws IOException
+    {
+        File file = new File("");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
     }
 
     @Test
@@ -32,6 +66,9 @@ class BaggageSortingUnitTest {
 
     @Test
     void emptyDestinationBox() {
+        baggageSortingUnit.emptyDestinationBox();
+        assertEquals(baggageList, baggageSortingUnit.getBaggageDepot().getBaggageList());
+        assertEquals(0, baggageSortingUnit.getDestinationBox().getBaggageList().size());
     }
 
     @Test
