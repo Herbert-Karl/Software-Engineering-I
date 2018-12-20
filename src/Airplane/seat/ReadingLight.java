590b107e308a47fa277e81d8096a299d6554cc75
DrCrazy1 <45233714+DrCrazy1@users.noreply.github.com>
2018-12-20 22:54:57 +0100

    Extracted ReadingLight form Sign

--- src/Airplane/seat/ReadingLight.java
+++ src/Airplane/seat/ReadingLight.java
@@ -1,10 +1,31 @@
 package Airplane.seat;
 
-public class ReadingLight extends Sign {
+import Airplane.airplanePart.IAirplanePart;
 
+public class ReadingLight implements IReadingLight {
+
+    private IAirplanePart light;
+    private boolean isOn;
     // constructor
-    public ReadingLight(String ID, String type)
+    public ReadingLight(String ID)
     {
-        setSign(ID, type, "ReadingLight");
+        light = new AirplanePart();
+        light.setAll(ID, "ReadingLight");
+        isOn = false;
+    }
+    
+    @Override
+    public void on() {
+        isOn = true;
+    }
+    
+    @Override
+    public void off() {
+        isOn = false;
+    }
+    
+    @Override
+    public String version() {
+        return light.version();
     }
 }
