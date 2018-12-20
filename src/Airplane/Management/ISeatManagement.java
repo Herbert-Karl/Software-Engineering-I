4373246c3bade1baa6f6a26b81af14559a1bd656
Blackswater <Raphaellawo@gmx.de>
2018-12-20 20:46:40 +0100

    change count

--- src/Airplane/Management/ISeatManagement.java
+++ src/Airplane/Management/ISeatManagement.java
@@ -1,5 +1,6 @@
 package Airplane.Management;
 
+import Airplane.Aircraft.Airplane;
 import Airport.Base.Passenger;
 
 import java.util.ArrayList;
@@ -7,8 +8,8 @@ import java.util.ArrayList;
 public interface ISeatManagement {
     String version();
     int assign(ArrayList<Passenger> passengers);
-    int countAvailableSeat();
-    int countAvailableSeat(String ticketClassString);
+    int countAvailableSeat(Airplane airplane);
+    int countAvailableSeat(String ticketClassString, Airplane airplane);
     ArrayList<Seat> searchSeatByPassengerName(String name);
     Passenger searchPassengerBySeatID(String seatID);
     void printPassengerList(SeatManagementSortCriteria sortCriteria);
--- src/Airplane/Management/SeatManagement.java
+++ src/Airplane/Management/SeatManagement.java
@@ -54,15 +54,26 @@ public class SeatManagement implements ISeatManagement{
 
     }
 
-    public int countAvailableSeat() {
+    public int countAvailableSeat(Airplane airplane) {
+        int seats = airplane.getBody().getFirstClassSeatArrayList().size();
+        seats += airplane.getBody().getBusinessClassSeatArrayList().size();
+        seats += airplane.getBody().getTouristClassSeatArrayList().size();
 
+        return seats;
     //Class Airplane legt an, wieviel First, Business und Touristclass Seats es gibt
 
     }
 
-    public int countAvailableSeat(String ticketClassString) {
+    public int countAvailableSeat(String ticketClassString, Airplane airplane) {
+        int seats = 0;
+        if(airplane.getBody().getFirstClassSeatArrayList().contains(ticketClassString))
+            seats++;
+        if(airplane.getBody().getBusinessClassSeatArrayList().contains(ticketClassString))
+            seats++;
+        if(airplane.getBody().getTouristClassSeatArrayList().contains(ticketClassString))
+            seats++;
 
-
+        return seats;
     }
 
     public Passenger searchSeatByPassengerName(String name) {
