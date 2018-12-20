56048e8a43ec074868131aaa32f51aae3ba706c0
DrCrazy1 <45233714+DrCrazy1@users.noreply.github.com>
2018-12-20 23:06:20 +0100

    Updated Constructor

--- src/Airplane/seat/TouristClassSeat.java
+++ src/Airplane/seat/TouristClassSeat.java
@@ -3,8 +3,8 @@ package Airplane.seat;
 public class TouristClassSeat extends Seat {
 
     // constructor for this class
-    public TouristClassSeat(String ID, String type)
+    public TouristClassSeat(String ID)
     {
-        setSeat(ID, type, "TouristClassSeat");
+        super(ID, "TouristClassSeat");
     }
 }
