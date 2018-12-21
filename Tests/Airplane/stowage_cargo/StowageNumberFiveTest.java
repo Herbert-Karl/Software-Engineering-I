0493e8bffd523657dfc184a90e949712a2f47a6f
fpoh <fabianpohlink@gmx.de>
2018-12-21 21:39:12 +0100

    UnitTest

--- Tests/Airplane/stowage_cargo/StowageNumberFiveTest.java
+++ Tests/Airplane/stowage_cargo/StowageNumberFiveTest.java
@@ -1,21 +1,40 @@
 package Airplane.stowage_cargo;
 
+import Airport.Base.*;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StowageNumberFiveTest extends TestCase {
+    private Container container;
+    private StowageNumberFive stowageNumberFive;
 
     @Test
     void load() {
-    }
+        stowageNumberFive = new StowageNumberFive();
+        Stack<Baggage> baggageStack = new Stack<>();
+        NormalBaggage normalBaggage = new NormalBaggage("content", 100, null);
+        baggageStack.push(normalBaggage);
 
-    @Test
-    void unload() {
+        container = new Container(ContainerType.AKH, "id", ContainerCategory.Normal, null,
+                "barcode", "qrcode", baggageStack);
+
+        assertTrue(stowageNumberFive.load(container));
+        assertFalse(stowageNumberFive.load(container));
     }
 
     @Test
     void weightOfStowageNumberFive() {
+        assertEquals(container.getWeight(), stowageNumberFive.weightOfStowageNumberFive());
     }
+
+    @Test
+    void unload() {
+        assertEquals(container, stowageNumberFive.unload());
+        assertEquals(null, stowageNumberFive.unload());
+    }
+
 }
