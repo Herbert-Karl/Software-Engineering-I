048d5eec6a3ef5b8cae2b29af1fa681e62dcb5d9
Hannes <hannesp1009@gmail.com>
2018-12-21 23:32:32 +0100

    Final version

--- src/Airport/Federal_Police/FederalPolice.java
+++ src/Airport/Federal_Police/FederalPolice.java
@@ -1,10 +1,8 @@
 package Airport.Federal_Police;
 
+import Airport.Base.*;
+import Airport.Airport.Airport;
 import Airport.Airport.Runway;
-import Airport.Base.AlarmType;
-import Airport.Base.Baggage;
-import Airport.Base.Item;
-import Airport.Base.Passenger;
 import Airport.Security_Check.IAutomaticBoardingPassControl;
 
 import java.util.ArrayList;
@@ -12,34 +10,100 @@ import java.util.ArrayList;
 public class FederalPolice implements IFederalPolice {
     private String uuid;
     private FederalPoliceResourcePool resourcePool;
-    private ArrayList arrestedPassengers = new ArrayList<Passenger>();
-    private ArrayList keepSafeBaggageList = new ArrayList<Baggage>();
-    private ArrayList keepSafeItemList = new ArrayList<Item>();
+    private ArrayList<Passenger> arrestedPassenger;
+    private ArrayList<Baggage> keepSafeBaggageList;
+    private ArrayList<Item> keepSafeItemList;
     private int numberOfWeaponInspected;
+
+    public FederalPolice(FederalPoliceResourcePool resourcePool, ArrayList<Passenger> arrestedPassenger, ArrayList<Baggage> keepSafeBaggageList, ArrayList<Item> keepSafeItemList, int numberOfWeaponInspected, int numberOfMunitionInspected) {
+        this.uuid = java.util.UUID.randomUUID().toString();
+        this.resourcePool = resourcePool;
+        this.arrestedPassenger = arrestedPassenger;
+        this.keepSafeBaggageList = keepSafeBaggageList;
+        this.keepSafeItemList = keepSafeItemList;
+        this.numberOfWeaponInspected = numberOfWeaponInspected;
+        this.numberOfMunitionInspected = numberOfMunitionInspected;
+    }
+
     private int numberOfMunitionInspected;
 
-    @Override
-    public void alarm(Runway runway, AlarmType alarmType) {
+    public String getUuid() {
+        return uuid;
+    }
 
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public FederalPoliceResourcePool getResourcePool() {
+        return resourcePool;
+    }
+
+    public void setResourcePool(FederalPoliceResourcePool resourcePool) {
+        this.resourcePool = resourcePool;
+    }
+
+    public ArrayList<Passenger> getArrestedPassenger() {
+        return arrestedPassenger;
+    }
+
+    public void setArrestedPassenger(ArrayList<Passenger> arrestedPassenger) {
+        this.arrestedPassenger = arrestedPassenger;
+    }
+
+    public ArrayList<Baggage> getKeepSafeBaggageList() {
+        return keepSafeBaggageList;
+    }
+
+    public void setKeepSafeBaggageList(ArrayList<Baggage> keepSafeBaggageList) {
+        this.keepSafeBaggageList = keepSafeBaggageList;
+    }
+
+    public ArrayList<Item> getKeepSafeItemList() {
+        return keepSafeItemList;
+    }
+
+    public void setKeepSafeItemList(ArrayList<Item> keepSafeItemList) {
+        this.keepSafeItemList = keepSafeItemList;
+    }
+
+    public int getNumberOfWeaponInspected() {
+        return numberOfWeaponInspected;
+    }
+
+    public void setNumberOfWeaponInspected(int numberOfWeaponInspected) {
+        this.numberOfWeaponInspected = numberOfWeaponInspected;
+    }
+
+    public int getNumberOfMunitionInspected() {
+        return numberOfMunitionInspected;
+    }
+
+    public void setNumberOfMunitionInspected(int numberOfMunitionInspected) {
+        this.numberOfMunitionInspected = numberOfMunitionInspected;
     }
 
     @Override
     public void alarm(IAutomaticBoardingPassControl automaticBoardingPassControl) {
-
+        automaticBoardingPassControl.closeDoors();
     }
 
     @Override
-    public void assignFederalPoliceOfficer(IFederalPoliceCar federalPoliceCar) {
-
+    public void alarm(Runway runway, AlarmType alarmType) {
+        runway.setClearedForTakeOff(false);
+        runway.setClearedToLand(false);
     }
 
     @Override
-    public void receive(ArrayList<Baggage> baggageList) {
-
+    public void assignFederalPoliceOfficer(FederalPoliceCar federalPoliceCar, FederalPoliceOfficer federalPoliceOfficer) {
+        ArrayList<FederalPoliceOfficer> x = federalPoliceCar.getFederalPoliceOfficerList();
+        x.add(federalPoliceOfficer);
+        federalPoliceCar.setFederalPoliceOfficerList(x);
     }
+
 
     @Override
     public void notifyGroundOperations(FederalPoliceReceipt federalPoliceReceipt) {
-
+        Airport.getInstance().getGroundOperationsCenter().receive(federalPoliceReceipt);
     }
 }
--- src/Airport/Federal_Police/FederalPoliceOfficer.java
+++ src/Airport/Federal_Police/FederalPoliceOfficer.java
@@ -2,40 +2,160 @@ package Airport.Federal_Police;
 
 import Airport.Base.*;
 import Airport.Security_Check.ISecurityCheck;
+import Airport.Security_Check.PassportControl;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 public class FederalPoliceOfficer {
+    private FederalPolice federalPolice;
     private String uuid;
-    private int id;
+    private String id;
     private String name;
     private Gender gender;
     private IDCard idCard;
     private boolean hasMachineGun;
     private ISecurityCheck securityCheck;
-
-    public boolean verify(Passport passport){
-        return false;
-    }
-    public boolean scan(Passport passport){
-        return  false;
-    }
-    public boolean inspectWeapon(BulkyBaggage specialGood){
-        return  false;
-    }
-    public boolean inspectMunition(BulkyBaggage specialGood){
-        return  false;
-    }
-    public void keepSafe(Baggage baggage){
-
-    }
-    public void keepSafe(Item item){
-
-    }
-    public void arrest(Passenger passenger){
-
+    Random rnd = new Random();
+    public FederalPoliceOfficer(String name, Gender gender, IDCard idCard, boolean hasMachineGun, ISecurityCheck securityCheck) {
+        this.uuid = java.util.UUID.randomUUID().toString();
+        this.id = "" + (rnd.nextInt(100000)+1);
+        this.name = name;
+        this.gender = gender;
+        this.idCard = idCard;
+        this.hasMachineGun = hasMachineGun;
+        this.securityCheck = securityCheck;
     }
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
     public IDCard getIdCard() {
         return idCard;
     }
+
+    public void setIdCard(IDCard idCard) {
+        this.idCard = idCard;
+    }
+
+    public boolean isHasMachineGun() {
+        return hasMachineGun;
+    }
+
+    public void setHasMachineGun(boolean hasMachineGun) {
+        this.hasMachineGun = hasMachineGun;
+    }
+
+    public ISecurityCheck getSecurityCheck() {
+        return securityCheck;
+    }
+
+    public void setSecurityCheck(ISecurityCheck securityCheck) {
+        this.securityCheck = securityCheck;
+    }
+
+    public boolean verify(Passport passport){
+        PassportControl pc = new PassportControl(id);
+        if(!pc.verify(passport)){
+            arrest(passport.getPassenger());
+        return true;
+        }
+        else
+            return false;
+
+    }
+    public boolean scan(Passport passport){
+        PassportControl pc = new PassportControl(id);
+        if(pc.scan(passport)){
+            return verify(passport);
+        }
+        else
+            return false;
+    }
+    public boolean inspectWeapon(BulkyBaggage bulkyBaggage){
+        if( Pattern.compile(Pattern.quote("glock7"), Pattern.CASE_INSENSITIVE).matcher(bulkyBaggage.getContent()).find() |
+                Pattern.compile(Pattern.quote("drugs"), Pattern.CASE_INSENSITIVE).matcher(bulkyBaggage.getContent()).find() |
+                Pattern.compile(Pattern.quote("knife"), Pattern.CASE_INSENSITIVE).matcher(bulkyBaggage.getContent()).find() |
+                Pattern.compile(Pattern.quote("exp!os!ve"), Pattern.CASE_INSENSITIVE).matcher(bulkyBaggage.getContent()).find()){
+            keepSafe(bulkyBaggage);
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean inspectMunition(BulkyBaggage bulkyBaggage){
+        if(Pattern.compile(Pattern.quote("munition"),Pattern.CASE_INSENSITIVE).matcher(bulkyBaggage.getContent()).find()){
+            keepSafe(bulkyBaggage);
+            return true;
+        }
+        else
+            return false;
+    }
+    public void keepSafe(Baggage baggage){
+        ArrayList<Baggage> baggageArrayList = new ArrayList<>();
+        baggageArrayList = federalPolice.getKeepSafeBaggageList();
+        baggageArrayList.add(baggage);
+        federalPolice.setKeepSafeBaggageList(baggageArrayList);
+    }
+    public void keepSafe(Item item){
+
+        ArrayList<Item> itemArrayList = new ArrayList<>();
+        itemArrayList = federalPolice.getKeepSafeItemList();
+        itemArrayList.add(item);
+        federalPolice.setKeepSafeItemList(itemArrayList);
+    }
+    public void arrest(Passenger passenger){
+        ArrayList<Passenger> passengerArrayList = new ArrayList<>();
+        passengerArrayList = federalPolice.getArrestedPassenger();
+        passengerArrayList.add(passenger);
+        federalPolice.setArrestedPassenger(passengerArrayList);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
