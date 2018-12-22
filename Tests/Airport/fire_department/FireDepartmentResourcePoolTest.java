144eea3012d1705e9c32bd66ad79d0421b79d2dc
tomislafv <tom.scholl55@gmail.com>
2018-12-22 02:24:39 +0100

    Update FireDepartmentResourcePoolTest.java

--- Tests/Airport/fire_department/FireDepartmentResourcePoolTest.java
+++ Tests/Airport/fire_department/FireDepartmentResourcePoolTest.java
@@ -4,9 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class FireDepartmentResourcePoolTest {
 
     @Test
     void getFireTrucksList() {
+        for (FireTruck fireTruck : <FireTruck>){
+            testFireTruckList.add(new FireTruck(firetruck))
+        }
+        fireTrucklist = getFireTrucksList();
+
+            assertEquals(fireTrucksList,testFireTruckList)
     }
 }
