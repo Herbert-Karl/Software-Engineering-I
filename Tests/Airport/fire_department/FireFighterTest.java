21021586e4108c636bda2b5b4e05d9a3f90adbf2
tomislafv <tom.scholl55@gmail.com>
2018-12-22 11:33:02 +0100

    Update FireFighterTest.java

--- Tests/Airport/fire_department/FireFighterTest.java
+++ Tests/Airport/fire_department/FireFighterTest.java
@@ -3,5 +3,7 @@ package Airport.fire_department;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FireFighterTest {
+  
+  // nur getter und setter methoden in Klasse daher testen wir nix
 
 }
